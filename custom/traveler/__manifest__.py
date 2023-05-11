a7d6ed756c151541e77e7749a8fec9aa28dd3554
Huyhoclaptrinh <giahuyphung282@gmail.com>
2023-05-11 11:04:55 +0700

    First module

---
+++ custom/traveler/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': "Traveler",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Aether",
+    'category': 'Adventure',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+        'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+    'license': 'LGPL-3',
+}
