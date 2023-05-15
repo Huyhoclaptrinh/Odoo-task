a43abeebc9e94673919e40a3e7c6a7ac1dee6ab9
Huyhoclaptrinh <giahuyphung282@gmail.com>
2023-05-15 16:30:42 +0700

    Update and using travel

--- custom/travel/models/travel_mem.py
+++ custom/travel/models/travel_mem.py
@@ -7,3 +7,17 @@ class MemberModel(models.Model):
     member_name = fields.Char()
     member_phone_num = fields.Integer()
     member_point = fields.Float()
+    availability_date = fields.Date(string="Availability Date", copy=False, default=lambda self: fields.Datetime.today())
+    selling_price = fields.Float(string="Selling Price", copy=False, readonly=True)
+    bedroom = fields.Integer(default=2)
+    active = fields.Boolean(string='Active', default=True)
+
+    STATE_SELECTION = [
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('canceled', 'Canceled'),
+    ]
+
+    state = fields.Selection(STATE_SELECTION, string='State', default='new', required=True, copy=False)
